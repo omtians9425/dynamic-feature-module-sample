2a2c72879811c9dacd7db05a97c9db28016b1d12
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-25 13:35:26 +0900

    check whether module is installed or not

--- app/src/main/java/com/google/android/samples/dynamicfeatures/MainActivity.kt
+++ app/src/main/java/com/google/android/samples/dynamicfeatures/MainActivity.kt
@@ -55,21 +55,27 @@ class MainActivity : AppCompatActivity() {
         initializeViews()
     }
 
+    private val moduleAssets by lazy { getString(R.string.module_assets) }
+
     /** Display assets loaded from the assets feature module. */
     private fun displayAssets() {
-        // Get the asset manager with a refreshed context, to access content of newly installed apk.
-        val assetManager = createPackageContext(packageName, 0).assets
-        // Now treat it like any other asset file.
-        val assets = assetManager.open("assets.txt")
-        val assetContent = assets.bufferedReader()
-                .use {
-                    it.readText()
-                }
+        if (manager.installedModules.contains(moduleAssets)) {
+            // Get the asset manager with a refreshed context, to access content of newly installed apk.
+            val assetManager = createPackageContext(packageName, 0).assets
+            // Now treat it like any other asset file.
+            val assets = assetManager.open("assets.txt")
+            val assetContent = assets.bufferedReader()
+                    .use {
+                        it.readText()
+                    }
 
-        AlertDialog.Builder(this)
-                .setTitle("Asset content")
-                .setMessage(assetContent)
-                .show()
+            AlertDialog.Builder(this)
+                    .setTitle("Asset content")
+                    .setMessage(assetContent)
+                    .show()
+        } else {
+            toastAndLog("The assets module is not installed")
+        }
     }
 
     /** Launch an activity by its class name. */
